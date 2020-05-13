27493202e5741988cb92359c8732bbcef36da453
Pasha Pourmand <pasha.pourmand@gmail.com>
2020-05-12 23:48:07 -0700

    create cli and option to display tags

---
+++ pyedm/main.py
@@ -0,0 +1,18 @@
+import eyed3
+import os 
+import click
+
+@click.command()
+@click.option("--get-tag", is_flag=True, help="get tag of a song")
+@click.argument("filename")
+def cli(get_tag, filename):
+    if get_tag:
+        try:
+            audiofile = eyed3.load(filename)
+            click.echo("Title: {}".format(audiofile.tag.title))
+            click.echo("Artist: {}".format(audiofile.tag.artist))
+            click.echo("Album: {}".format(audiofile.tag.album))
+            click.echo("Genre: {}".format(audiofile.tag.genre))
+            click.echo("BPM: {}".format(audiofile.tag.bpm))
+        except IOError as e:
+            click.echo("File unable to be found" + e)

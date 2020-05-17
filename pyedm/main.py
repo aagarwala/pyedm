e7234bffad3f814f8dc8a6ca958426bf132afb62
Pasha Pourmand <pasha.pourmand@gmail.com>
2020-05-16 22:02:18 -0700

    get song info from beatport

--- pyedm/main.py
+++ pyedm/main.py
@@ -48,7 +48,7 @@ def get_song_webpage(song_info):
             genre = result.find('.buk-track-genre', first=True).text
             release_date = result.find('.buk-track-released', first=True).text
             url = list(result.find('.buk-track-title', first=True).absolute_links)[0]
-            results_map[i] = Song(full_title, artist_list, label, genre, release_date, url)
+            results_map[i] = Song(full_title, artist_list, url)
 
             click.echo("{}. Title: {} | Artist: {} | Label: {} | Genre: {} | Released: {}".format(i, full_title, artist_list, label, genre, release_date))
     
@@ -60,25 +60,54 @@ def get_song_webpage(song_info):
         click.echo("Sorry that's not a choice")
         return
 
-    results_map[song_choice].print_song_info()
+    print("Title: {} Artist: {}".format(results_map[song_choice].title, results_map[song_choice].artist_list))
 
     if click.confirm('Open webpage first?'):
         webbrowser.open(results_map[song_choice].url)
 
     if click.confirm('Do you want to tag the file?'):
-        click.echo('Excellent! Tagging now')
+        song_to_tag = get_song_info(results_map[song_choice])
+        song_to_tag.print_song_info()
 
-def open_song_url(song_url):
-    webbrowser.open(song_url)
+def get_song_info(song):
+    session = HTMLSession()
+    r = session.get(song.url)
+    
+    info_items = r.html.find('.interior-track-content-list')[0]
+    
+    length = info_items.find('.interior-track-length', first=True).find('.value', first=True).text
+    released_date = info_items.find('.interior-track-released', first=True).find('.value', first=True).text
+    bpm = info_items.find('.interior-track-bpm', first=True).find('.value', first=True).text
+    genre = info_items.find('.interior-track-genre', first=True).find('.value', first=True).text
+    labels_element = info_items.find('.interior-track-labels', first=True).find('.value')
+    labels_list = []
+
+    for l in labels_element:
+        labels_list.append(l.text)
+
+    labels = ", ".join(labels_list)
+    
+    song_to_tag = Song(song.title, song.artist_list, song.url)
+    song_to_tag.labels = labels
+    song_to_tag.genre = genre
+    song_to_tag.release_date = released_date
+    song_to_tag.bpm = bpm
+    song_to_tag.length = length
+
+    return song_to_tag
 
 class Song:
-    def __init__(self, title, artist_list, label, genre, release_date, url):
+    def __init__(self, title, artist_list, url):
         self.title = title
         self.artist_list = artist_list
-        self.label = label
-        self.genre = genre
-        self.release_date = release_date
+        self.labels = ""
+        self.genre = ""
+        self.release_date = ""
         self.url = url
+        self.length = ""
+        self.bpm = ""
+    
+
     
     def print_song_info(self):
-        click.echo("Title: {} | Artist: {} | Label: {} | Genre: {} | Released: {}".format(self.title, self.artist_list, self.label, self.genre, self.release_date))
+        click.echo("Title: {} | Artist: {} | Label: {} | Genre: {} | Released: {}".format(self.title, self.artist_list, self.labels, self.genre, self.release_date))

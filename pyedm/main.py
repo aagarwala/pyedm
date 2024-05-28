c3d5fdba7bc2506fbd2eadd225ac18e9fbc086a9
Anirudh <agarwala.anirudh@gmail.com>
2024-05-28 11:36:33 -0700

    updated html scraping for beatport.com redesign

--- pyedm/main.py
+++ pyedm/main.py
@@ -1,14 +1,14 @@
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3, APIC
-from mutagen import MutagenError
 import os 
 import click
-from requests_html import HTMLSession
-import webbrowser
+from requests_html import AsyncHTMLSession
 import re
 import time
 import datetime
 import requests
+import json
+import asyncio
 
 SKIP_FOLDER = ["Music", "iTunes", "AAnster"]
 
@@ -20,7 +20,7 @@ and stops if it goes past that number or if it reaches MAX_SONGS, whichever come
 @click.option("-N", "--no_confirmation", is_flag=True, help="if this option is set, then there is no confirmation on tagging a song")
 @click.argument("max_songs")
 @click.argument("music_library_path", envvar='PYEDM_LIB_PATH')
-def cli(show_tags, lookback, no_confirmation, max_songs, music_library_path):
+def cli(lookback, no_confirmation, max_songs, music_library_path):
     """
     \\\\ Introduction \\\\
 
@@ -35,7 +35,7 @@ def cli(show_tags, lookback, no_confirmation, max_songs, music_library_path):
 
     def pretty_time(epoch_time):
         """
-        small little function that returns a readable date/time string
+        small little function that returns a readable date/time string 
         """
 
         return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_time))
@@ -63,6 +63,9 @@ def cli(show_tags, lookback, no_confirmation, max_songs, music_library_path):
     total_tracks = "\n".join(list_of_tracks)
     click.echo("List of {} tracks to be tagged:".format(len(list_of_tracks)))
     click.echo(total_tracks)
+    # Check to make sure you want to proceed on given songs
+    if not click.confirm('Do you want to proceed?'):
+        return 0 # exit
     for track in list_of_tracks:
         audiofile = {}
         match = re.search(r"([^/]*) - (.*)\.mp3", track)
@@ -100,6 +103,7 @@ def search_latest(music_library_path, max_songs, lookback, list_of_tracks):
 
 def get_song_webpage(song_title, song_artist, filename, no_confirmation):
     song_choice = None
+    results_map = {}
     # Put together title and artist for search
     song_info = "{} {}".format(song_title, song_artist)
 
@@ -118,32 +122,61 @@ def get_song_webpage(song_title, song_artist, filename, no_confirmation):
 
     song_artist = song_artist.split(",")
     
-
-    session = HTMLSession()
-    r = session.get('https://www.beatport.com/search/tracks?q={}&per-page=25'.format(song_info.replace(" ", "%20").replace("&", "")))
-    search_results = r.html.find('.bucket-item')
-    # Sort the results by release date as to get the first release (versus re-release)
-    search_results.sort(key=lambda x: x.find('.buk-track-released', first=True).text)
-    results_map = {}
     
-    click.echo()
+    async def scrape_beatport():
+        # start async html session
+        session = AsyncHTMLSession()
+        click.echo('https://www.beatport.com/search/tracks?q={}&per-page=25'.format(song_info.replace(" ", "%20").replace("&", "")))
+        r = await session.get('https://www.beatport.com/search/tracks?q={}&per-page=25'.format(song_info.replace(" ", "%20").replace("&", "")))
+         # Wait for JavaScript to render
+        await r.html.arender()
 
-    with click.progressbar(search_results) as bar:
-        for i, result in enumerate(bar):
-            # find titles and/or remix
-            primary_title = result.find('.buk-track-primary-title', first=True).text
-            secondary_title = result.find('.buk-track-remixed', first=True).text
-            full_title = "{} ({})".format(primary_title, secondary_title)
+        # returns the html elements of all the track lists
+        tracks = r.html.find('[data-testid="tracks-list-item"]')
 
-            # find artists
-            artists = result.find('.buk-track-artists')[0].text
-            artist_list = artists.split(",")
+        # Extract the JSON data from the HTML
+        json_data = None
+        json_text = r.html.find('#__NEXT_DATA__', first=True).text
+        json_data = json.loads(json_text)
 
-            label = result.find('.buk-track-labels', first=True).text
-            genre = result.find('.buk-track-genre', first=True).text
-            release_date = result.find('.buk-track-released', first=True).text
-            url = list(result.find('.buk-track-title', first=True).absolute_links)[0]
-            results_map[i] = Song(full_title, artists, url)
+        # Check if JSON data is found
+        if json_data is None:
+            click.echo("JSON data not found")
+            return
+
+         # this returns the list of results (150) we only need first 25
+        json_track_results = json_data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']['data'][0:25]
+        click.echo()
+
+        for i, track in enumerate(tracks):
+            # Return track url reference
+            url_ref = track.find('.Lists-shared-style__MetaRow-sc-b8c20e45-4.bWa-dwr', first=True).find('a', first=True).attrs['href']
+            # Create the full url to the track (we will call this later to get more info)
+            track_url = "https://www.beatport.com{}".format(url_ref)
+            # print(f"Track URL: {track_url}")
+
+            # This gets both the track name as well as the mix name for a full title
+            track_name = json_track_results[i]['track_name']
+            mix_name = json_track_results[i]['mix_name']
+            full_title = "{} ({})".format(track_name, mix_name)
+
+            # This returns a list of the artists
+            artist_list = [artist['artist_name'] for artist in json_track_results[i]['artists']]
+            # This combines the artist into a string that can be entered when tagging the track later
+            artists = ', '.join(artist_list)
+
+            # return the label name
+            label = json_track_results[i]['label']['label_name']
+            # return just the first genre
+            genre = json_track_results[i]['genre'][0]['genre_name']
+            release_date = json_track_results[i]['release_date']
+            match = re.search(r"(.*)T", release_date)
+            if match:
+                release_date = match.group(1)
+            else:
+                click.echo(f"ERROR: Need to fix the regex for release_date: {release_date}")
+ 
+            results_map[i] = Song(full_title, artists, track_url)
 
             # Get rid of any feat. artist in full title 
             match = re.search(r"(.*)\s(feat\.|ft\.).*(\(.*)", full_title)
@@ -165,7 +198,8 @@ def get_song_webpage(song_title, song_artist, filename, no_confirmation):
 
             # Check to see if song exact match, if so then break from results and automatically choose number
             if song_title.lower() == stripped_full_title.lower() and artist_match(song_artist, artist_list):
-                print("*** Song automatically chosen ***")
+                click.echo("*** Song automatically chosen ***")
+                nonlocal song_choice
                 song_choice = i
                 click.echo()
                 click.echo("{}. Title: {} | Artist: {} | Label: {} | Genre: {} | Released: {}".format(i, full_title, artists, label, genre, release_date))
@@ -173,6 +207,12 @@ def get_song_webpage(song_title, song_artist, filename, no_confirmation):
                 break
 
             click.echo("{}. Title: {} | Artist: {} | Label: {} | Genre: {} | Released: {}".format(i, full_title, artists, label, genre, release_date))
+
+        # this closes the session
+        await session.close()
+
+    # get the search results
+    asyncio.run(scrape_beatport())
 
     if song_choice is None:
         click.echo()
@@ -191,66 +231,71 @@ def get_song_webpage(song_title, song_artist, filename, no_confirmation):
     # Only ask about tagging file if no_confirmation is not set (default behavior)
     if not no_confirmation:
         if click.confirm('Do you want to tag the file?'):
+            try:
+                song_to_tag = get_song_info(results_map[song_choice])
+                tag_song(song_to_tag, filename)
+            except:
+                print("Wasn't able to tag this song")
+    else:
+        try:
             song_to_tag = get_song_info(results_map[song_choice])
             tag_song(song_to_tag, filename)
-    else:
-        song_to_tag = get_song_info(results_map[song_choice])
-        tag_song(song_to_tag, filename)
+        except:
+            print("Wasn't able to tag this song")
+
 
 def get_song_info(song):
-    session = HTMLSession()
-    r = session.get(song.url)
-    
-    info_items = r.html.find('.interior-track-content-list')[0]
-    
-    length = info_items.find('.interior-track-length', first=True).find('.value', first=True).text
-    released_date = info_items.find('.interior-track-released', first=True).find('.value', first=True).text
-    match = re.match(r"(\d+)-(\d+)-(\d+)", released_date)
-    if match:
-        year = match.group(1)
-    bpm = info_items.find('.interior-track-bpm', first=True).find('.value', first=True).text
-    genre = info_items.find('.interior-track-genre', first=True).find('.value', first=True).text
-    labels_element = info_items.find('.interior-track-labels', first=True).find('.value')
-    labels_list = []
-
-    for l in labels_element:
-        labels_list.append(l.text)
-
-    labels = ", ".join(labels_list)
-
-    # if theres an album
-    album_url = list(r.html.find('.interior-track-releases', first=True).find('.interior-track-release-artwork-link',first=True).absolute_links)[0]
-    album_page = session.get(album_url)
-    album_page_info = album_page.html.find('.interior-release-chart-content', first=True)
-    album_name = album_page_info.find('h1', first=True).text
-    # Gets album art
-    album_artwork_url = album_page.html.find('.interior-release-chart-artwork-parent', first=True).find('img')[0].attrs['src']
-    response = requests.get(album_artwork_url)
-    album_artwork = response.content
-
-    # If there are mulitple songs, need to match to right one to get track number / total tracks
-    album_tracks = album_page.html.find(".bucket-items", first=True).find(".bucket-item")
-    for track in album_tracks:
-        # Frist need to compare full title to see which track it is
-        primary_title = track.find(".buk-track-title", first=True).find(".buk-track-primary-title", first=True).text
-        secondary_title = track.find(".buk-track-title", first=True).find(".buk-track-remixed", first=True).text
-        full_title = "{} ({})".format(primary_title, secondary_title)
-        if song.title.lower() == full_title.lower():
-            track_number = track.find(".bucket-item", first=True).attrs['data-ec-position'] # gets the track number
-            break
-    track_number = "{}/{}".format(track_number, str(len(album_tracks)))
-
     song_to_tag = Song(song.title, song.artist_list, song.url)
-    song_to_tag.labels = labels
-    song_to_tag.genre = genre
-    song_to_tag.release_date = released_date
-    song_to_tag.bpm = int(bpm)
-    song_to_tag.length = length
-    song_to_tag.album_name = album_name
-    song_to_tag.album_artwork = album_artwork
-    song_to_tag.track_number = track_number
-    song_to_tag.year = year
 
+    async def scrape_beatport():
+        session = AsyncHTMLSession()
+
+        r = await session.get(song.url)
+
+        await r.html.arender()
+
+         # Extract the JSON data from the HTML
+        json_data = None
+        json_text = r.html.find('#__NEXT_DATA__', first=True).text
+        json_data = json.loads(json_text)
+
+        # Check if JSON data is found
+        if json_data is None:
+            click.echo("JSON data not found")
+            return
+
+        json_track_data = json_data['props']['pageProps']['dehydratedState']['queries'][0]['state']['data']
+
+        # tags all the necessary metadata
+        bpm = json_track_data['bpm']
+        genre = json_track_data['genre']['name']
+        labels = json_track_data['release']['label']['name']
+        album_name = json_track_data['release']['name']
+        # Gets album art
+        album_artwork_url = json_track_data['release']['image']['uri']
+        response = requests.get(album_artwork_url)
+        album_artwork = response.content
+
+        # gets track number
+        track_number = json_track_data['number']
+        # extracts the year from the publish date
+        match = re.search(r"(\d\d\d\d)", json_track_data['publish_date'])
+        if match:
+            year = match.group(1)
+        else:
+           click.echo(f"ERROR: Need to fix the regex for release_date: {year}")
+
+        song_to_tag.labels = labels
+        song_to_tag.genre = genre
+        song_to_tag.bpm = int(bpm)
+        song_to_tag.album_name = album_name
+        song_to_tag.album_artwork = album_artwork
+        song_to_tag.track_number = str(track_number)
+        song_to_tag.year = year
+
+        await session.close()
+
+    asyncio.run(scrape_beatport())
     return song_to_tag
 
 def tag_song(song_to_tag, song_file_path):
